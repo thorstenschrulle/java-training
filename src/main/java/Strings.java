7d5dc7bc3d2772d84b678a282a99f4da8994ddff
thorstenschrulle <thorsten.schrulle@googlemail.com>
2020-03-02 15:32:14 +0100

    zesz

--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -1,8 +1,8 @@
 public class Strings {
     public static void main (String[] args) {
-       String s = "some text";
-       String s2 = "some more text";
-       System.out.println(s.substring(2,7));
-        System.out.println(s);
+       String s = "some";
+       String s2 = "some";
+       // never compare strings with ==
+       System.out.println(s.equals(s2));
     }
 }
